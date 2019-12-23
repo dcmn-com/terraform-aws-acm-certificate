e145bb62918a9566f2e25e09579b262b6ed9acf6
v47 <victor.verg@hotmail.com>
2019-12-23 17:12:38 +0100

    Migration to terraform 0.12

--- main.tf
+++ main.tf
@@ -4,26 +4,26 @@ data "aws_route53_zone" "default" {
 }
 
 resource "aws_acm_certificate" "default" {
-  domain_name               = "${var.domain_name}"
+  domain_name               = var.domain_name
   validation_method         = "DNS"
-  subject_alternative_names = "${concat(var.alternative_domain_names, list(format("*.%s", var.domain_name)))}"
+  subject_alternative_names = concat(var.alternative_domain_names, list(format("*.%s", var.domain_name)))
 
-  tags = "${merge(
+  tags = merge(
     map(
       "Name", "${var.name}"
     ),
-  "${var.tags}")}"
+  "${var.tags}")
 }
 
 resource "aws_route53_record" "default" {
-  name    = "${aws_acm_certificate.default.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.default.domain_validation_options.0.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.default.id}"
+  name    = aws_acm_certificate.default.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.default.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.default.id
   records = ["${aws_acm_certificate.default.domain_validation_options.0.resource_record_value}"]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "default" {
-  certificate_arn         = "${aws_acm_certificate.default.arn}"
+  certificate_arn         = aws_acm_certificate.default.arn
   validation_record_fqdns = ["${aws_route53_record.default.fqdn}"]
 }
